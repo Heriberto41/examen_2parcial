1cdcbdf771bb39c7f22125b861a86b41edb0e1a4
Heriberto41 <1715110167@utec-tgo.edu.mx>
2017-03-09 16:00:36 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+
+    <?php
+       $db_host = "am1shyeyqbxzy8gc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+       $db_name = "jem52cv0hn307cd3";
+       $db_user = " k32fygihhm3c7rtr";
+       $db_password = "tywi4bftys2ma080";
+       $connection = mysqli_connect($db_host, $db_user, $db_password) or die("Connection Error: " . mysqli_error());
+        mysqli_select_db($connection, $db_name) or die("Error al seleccionar la base de datos:".mysqli_error());
+          @mysqli_query("SET NAMES 'utf8'");
+
+    $sql_query = "SELECT * FROM productos";
+    $result = mysqli_query($connection, $sql_query);
+    $rows = array();
+while($r = mysqli_fetch_assoc($result)) {
+  $rows[] = $r;
+}
+print json_encode($rows);
+?>
+
